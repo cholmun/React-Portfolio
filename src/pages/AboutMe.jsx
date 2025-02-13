11108beb5833f163384087b870bf85c91ca86bb3
cholmun <c.holmun55@gmail.com>
2025-02-12 23:32:22 -0500

    add AboutMe, Portfolio, and Resume pages; create Contact placeholder

---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="" alt="Developer" />
+      <p>Short bio (add later) </p>
+    </section>
+  );
+};
+
+export default AboutMe;
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Project from '../components/Project';
+
+const Portfolio = () => {
+  return (
+    <section>
+      <h2>Portfolio</h2>
+      <div>
+        {/* { projects.map((project, i) => (} */}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
