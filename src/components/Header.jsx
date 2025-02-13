35283a28b6b5d3ce0e80cedef39b155cdd542df1
cholmun <c.holmun55@gmail.com>
2025-02-12 23:25:22 -0500

    layout structure; add header, footer, and navigation components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Casey Holmun</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
